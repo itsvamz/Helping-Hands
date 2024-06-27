d8cb3e04b5b327bc46ed5fe732687843b7f6f2cd
itsvamz <vamika.mendiratta1304@gmail.com>
2024-06-27 18:57:40 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,3 +105,37 @@ document.getElementById('scrollLink3').addEventListener('click', function(event)
   // (prevent the URL from changing to "#" in the address bar)
   event.preventDefault();
 });
+
+//STORIES
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
